1ff52524166462d11ec92c8ad3a154a8643fdf96
buanxu <15274977794@163.com>
2021-05-10 15:54:53 +0800

    update src/main/java/com/buanxu/crm/CrmApplication.java.

--- src/main/java/com/buanxu/crm/CrmApplication.java
+++ src/main/java/com/buanxu/crm/CrmApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrmApplication {
 
     public static void main(String[] args) {
-        System.out.println("springboot");        
-        System.out.println("springboot");
-        System.out.println("test pull");
-    
         SpringApplication.run(CrmApplication.class, args);
     }
 }
