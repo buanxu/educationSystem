ad354e78855e8d02a9ea4ad3185ea9e2444872f6
buanxu <15274977794@163.com>
2021-05-10 15:06:22 +0800

    项目初始化

--- src/main/java/com/buanxu/crm/CrmApplication.java
+++ src/main/java/com/buanxu/crm/CrmApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrmApplication {
 
     public static void main(String[] args) {
+        System.out.println("开启项目");
         SpringApplication.run(CrmApplication.class, args);
     }
 
